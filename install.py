1ec26a8a970d79d6082226c92d8c50a929909780
Michael Deross <michael.a.deross@gmail.com>
2023-03-28 12:37:02 -0700

    testing new std functions, bugs are more present than I expected.

--- install.py
+++ install.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # I can't beleive this works.
 
+release = False;
+
 import os
 if os.system("if [[ `whoami` != \"root\" ]]\nthen\nexit 1\nfi") != 0:
     print("build error: root is required")
@@ -11,17 +13,25 @@ if os.system("if [[ `pwd` == \"*/Flaarc\" ]]\nthen\nexit 1\nfi") != 0:
     print("please CD into the directory of Flaarc");
     exit(1);
 
-
+cb = "cargo build"
+if release:
+    cb+=" --release"
 
 print("building flaarc...");
-if os.system("cargo build --release") != 0:
+if os.system(cb) != 0:
     print("Error during build phase")
     exit(1);
+
+r = "/target/release/flaarc"
+if release == False :r="/target/debug/flaarc"
+
+os.system("cp \"`pwd`" + r + "\" /bin/flaarc")
 
 os.system("mkdir /lib/flaarc")
 
 libfns = [
-        ["if equal.c", "if equal"]
+        ["if equal.c", "if equal"],
+        ["upper.c", "upper"]
         ]
 
 for i in libfns:
